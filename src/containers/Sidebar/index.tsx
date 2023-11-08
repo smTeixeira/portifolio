0131463ed46a6997e7ea0717196d5d1175c3ecc5
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-11-08 01:15:22 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/smTeixeira.png" />
-    <Titulo fontSize={20}> Samuel Teixeira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}> Samuel Teixeira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        smTeixeira
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
